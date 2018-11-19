b379ce2088519e214e3961b8711564dd6337cc93
Akram Mukasa <mukasaakram55@gmail.com>
2018-11-19 12:49:57 +0300

    add urls end points for signup

---
+++ SEND_IT/api/routes/urls.py
@@ -0,0 +1,23 @@
+"""
+This module handels requests to urls.
+"""
+from flask.views import MethodView
+from api.controllers.user_views import SignUp
+
+
+
+class Urls(object):
+    """
+    Class to generate urls
+    """
+      
+    @staticmethod
+    def generate_url(app):
+        """
+         Generates urls on the app context
+        :param: app: takes in the app variable
+        :return: urls
+        """
+        app.add_url_rule('/api/v1/auth/signup',
+                         view_func=SignUp.as_view('Signup'), methods=['POST',])
+        
